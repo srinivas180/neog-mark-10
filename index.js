024cbc3f60da4775a4aec0bdafb8da09d0930975
Srinivas <srinivas365@proton.me>
2022-10-03 10:56:47 +0530

    Added Full Logic in JS

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-var billAmount = 243;
-var cash = 2000;
+var billAmountHTML = document.querySelector("#bill-amount");
+var cashHTML = document.querySelector("#cash-given");
+var billAmount, cash;
 
+var btnCheck = document.querySelector("#btn-check");
 var currencies = [2000, 500, 100, 20, 10, 5, 1];
 var noOfNotes = [];
 
-var returnAmount = cash - billAmount;
-for(var i = 0; i < currencies.length; i++) {
-    var noOfCurrentNotes = Math.floor(returnAmount / currencies[i]);
-    noOfNotes.push(noOfCurrentNotes);
-    returnAmount = returnAmount - (currencies[i] * noOfCurrentNotes);
-    console.log(noOfNotes)
+function getChange() {
+    var returnAmount = cash - billAmount;
+    noOfNotes = [];
+    for(var i = 0; i < currencies.length; i++) {
+        var noOfCurrentNotes = Math.floor(returnAmount / currencies[i]);
+        noOfNotes.push(noOfCurrentNotes);
+        returnAmount = returnAmount - (currencies[i] * noOfCurrentNotes);
+    }
 }
+
+function fillNoOfNotesInHTML() {
+    var notesHTML = document.querySelectorAll(".notes");
+    notesHTML.forEach((note, index) => {
+        note.innerHTML = noOfNotes[index];
+    });
+}
+
+function clickHandler() {
+    billAmount = parseInt(billAmountHTML.value);
+    cash = parseInt(cashHTML.value);
+
+    if(billAmount === undefined || cash === undefined) {
+        console.log("Enter bill amount and cash given in edit boxes");
+    }
+    else if(billAmount > cash) {
+        console.log("Enter valid cash or bill amount. Bill Amount mush be lesser than or equal to cash given.");
+    }
+    else {
+        getChange();
+        fillNoOfNotesInHTML();
+    }
+}
+
+btnCheck.addEventListener("click", clickHandler);
